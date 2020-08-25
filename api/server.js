3822408ea54621403ee240b3a9b71f7840d18dba
Jessica <duellje@oregonstate.edu>
2020-08-24 20:47:50 -0400

    added dummy routes

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const authRouter = require("../auth/auth-router");
 const usersRouter = require("../users/usersRouter");
 const classesRouter = require("../classes/classesRouter");
 const studentsRouter = require("../students/studentsRouter");
+const dummyRouter = require("../dummyData/dummyRouter");
 
 server.use(express.json());
 server.use(cors());
@@ -15,6 +16,7 @@ server.use("/auth", authRouter);
 server.use("/users", authenticate, usersRouter);
 server.use("/classes", authenticate, classesRouter);
 server.use("/students", authenticate, studentsRouter);
+server.use("/dummy", dummyRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ server: "working" });
---
+++ dummyData/dummyRouter.js
@@ -0,0 +1,71 @@
+const router = require("express").Router();
+
+router.get("/users", (req, res) => {
+  const students = [{ name: "Neo" }, { name: "Trinity" }, { name: "Smith" }];
+  res.status(200).json({ data: students });
+});
+
+router.get("/classes", (req, res) => {
+  const classes = [{ name: "CS" }, { name: "Psy" }, { name: "Math" }];
+  res.status(200).json({ data: classes });
+});
+
+router.get("/classes/1", (req, res) => {
+  res.status(200).json({ data: { name: "CS" } });
+});
+
+router.get("/students/1/tasks", (req, res) => {
+  const tasks = [
+    {
+      id: 1,
+      name: "Determine a thesis",
+      description: "Pick a topic to research",
+      due_date: "Sep 1, 2020",
+      completed: 0,
+    },
+  ];
+
+  res.status(200).json({ data: tasks });
+});
+
+router.post("/users/classes", (req, res) => {
+  res.status(201).json({ message: "Success" });
+});
+
+router.post("/users/students", (req, res) => {
+  res.status(201).json({ message: "Success" });
+});
+
+router.post("/students/1/tasks", (req, res) => {
+  res.status(201).json({ message: "Success" });
+});
+
+router.put("/classes/1", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+router.put("/users/students/1", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+router.put("/students/1/tasks/1", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+router.delete("/users", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+router.delete("/classes/1", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+router.delete("/users/students/1", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+router.delete("/students/1/tasks/1", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+module.exports = router;
